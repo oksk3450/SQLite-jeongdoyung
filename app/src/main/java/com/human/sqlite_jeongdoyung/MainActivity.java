6e7f07192a3d8ddacaa2ef6e3c989abd73b8bac5
oksk3450@naver.com <oksk3450@naver.com>
2021-03-17 13:55:19 +0900

    20210317 결과

--- app/src/main/java/com/human/sqlite_jeongdoyung/MainActivity.java
+++ app/src/main/java/com/human/sqlite_jeongdoyung/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,36 +19,170 @@ import com.human.sqlite_jeongdoyung.DatabaseTables.StudentTable;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     //현재클래스에서 사용할 멤버변수 선언(아래)
     private DatabaseHelper mDatabaseHelper;
     private SQLiteDatabase mSqLiteDatabase;//sql템플릿(insert,select..)이 여기포함.
     private RecyclerAdapter mRecyclerAdapter;
     private List mItemList = new ArrayList<StudentVO>();//객체생성,셀렉트 쿼리결과 저장객체
+    //입력,수정,삭제 EditText 변수선언
+    private EditText mEditTextGrade;
+    private EditText mEditTextNumber;
+    private EditText mEditTextName;
+    //어댑터에서 선택한 값확인 변수
+    private int currentCursorId = -1;
+    //버튼 Button 변수선언
+    private Button mButtonInsert;
+    private Button mButtonUpdate;
+    private Button mButtonDelete;
+    //가상키보드제어하는 변수선언
+    private InputMethodManager mInputMethodManager;
+
+    //메인액티비티가 실행되면, 자동으로 실행되는 메서드 onCreate 입니다.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);//부모 클래스 초기화(메모리로딩)
+        setContentView(R.layout.activity_main);//화면 렌더링 됨.
         //신규 데이터베이스 객체 생성=메모리에 올리기=실행가능하게 만들기(아래)
         //=데이터베이스헬퍼클래스의 생성자 매서드 실행
         mDatabaseHelper = new DatabaseHelper(this,"school.db",null,1);
         //데이터베이스 파일 만들기(아래)
-        mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();
+        mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();//싱클톤으로 만들어진 매서드
+        //싱글톤만들었다는 의미는 인스턴스는 1번만 실행됨.즉, school.db파일이 있으면, 생성않됨.
         //테스트로 mSqLiteDatabse 객체를 이용해서 더미데이터 인서트 테스트
         //자바의 HashMap형식과 비슷한 안드로이드 데이터형 ContentsValues형
-        ContentValues contentValues = new ContentValues();
+        /*ContentValues contentValues = new ContentValues();
         contentValues.put(StudentTable.GRADE, 4);
         contentValues.put(StudentTable.NUMBER,20210004);
         contentValues.put(StudentTable.NAME,"아무개4");
         mSqLiteDatabase.insert(StudentTable.TABLE_NAME,null,contentValues);
+        */
         //mItemList에 셀렉트 쿼리 결과값이 셋 되어 있어야 함.
-
+        //멤버변수로 객체 초기화(아래)
+        bindObject();
         //List 실행 리사이클러 어댑터 바인딩(아래)
         bindList();//여서는 공간마련
-        //List 반영(화면출력)
+        //List 반영(화면출력: 입력,수정,삭제시 화면 리프레시가 필요하고, 구현하는 메서드)
         updateList();//여기에서 데이터바인딩되서 RecyclerAdaper가 화면에 재생됩니다.
+        //update버튼 클릭이벤트(아래)
+        btnUpdate();
+        //delete버튼 클릭이벤트(아래)
+        btnDelete();
+        //insert버튼 클릭이벤트(아래)
+        btnInsert();
     }
 
+    //EditText 콤포넌트 입력값 없애기
+    private void clearComponent() {
+        //EditText객체의 값 비우기
+        mEditTextGrade.setText("");
+        mEditTextNumber.setText("");
+        mEditTextName.setText("");
+        currentCursorId = -1;//현재 테이블커서ID가 지워졌으니, 초기화시킴
+    }
+    //insert버튼 클릭이벤트(아래)
+    private void btnInsert() {
+        mButtonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //"".equals()공백체크 && 오브젝트 == null체크 2개의 조건을 1개로 해결한 매서드 isEmpty() 로 개선.
+                if(mEditTextGrade.getText().toString().isEmpty() || "".equals(mEditTextNumber.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),"학년/학번 값은 필수 입니다.",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                final int grade = Integer.parseInt(mEditTextGrade.getText().toString());
+                final int number = Integer.parseInt(mEditTextNumber.getText().toString());
+                final String name = mEditTextName.getText().toString();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(StudentTable.GRADE,grade);
+                contentValues.put(StudentTable.NUMBER,number);
+                contentValues.put(StudentTable.NAME,name);
+                //insert쿼리 호출
+                insertData(contentValues);
+                //EditText 콤포넌트 입력값 없애기
+                clearComponent();
+                //화면 리프레스
+                updateList();
+                //키보드 숨기기
+                mInputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        });
+    }
+    //SQLiteDatabase 템플릿 insert메서드 실행
+    private void insertData(ContentValues contentValues) {
+        mSqLiteDatabase.insert(StudentTable.TABLE_NAME,null,contentValues);
+    }
+    //delete버튼 클릭이벤트(아래)
+    private void btnDelete() {
+        mButtonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentCursorId == -1) {
+                    Toast.makeText(getApplicationContext(),"선택된 값이 없습니다.",Toast.LENGTH_SHORT).show();
+                    return;//선택된 값이 없으면 삭제이벤트는 종료하기
+                }
+                //삭제쿼리 호출
+                deleteData(currentCursorId);
+                //EditText콤포넌트 값 없애기
+                clearComponent();
+                //리사이클러뷰 화면 리프레시(아래)
+                updateList();
+            }
+        });
+    }
+    //SQLiteDatabase 템플릿 delete메서드 실행
+    private void deleteData(int currentCursorId) {
+        mSqLiteDatabase.delete(StudentTable.TABLE_NAME,StudentTable._ID+"="+currentCursorId,null);
+    }
+
+    //update버튼 클릭이벤트(아래)
+    private void btnUpdate() {
+        mButtonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentCursorId == -1) {
+                    Toast.makeText(getApplicationContext(),"선택된 값이 없습니다.",Toast.LENGTH_SHORT).show();
+                    return;//클릭이벤트 진행중지
+                }
+                //DB갱신
+                final int grade = Integer.parseInt(mEditTextGrade.getText().toString());
+                final int number = Integer.parseInt(mEditTextNumber.getText().toString());
+                final String name = mEditTextName.getText().toString();
+                //쿼리메서드 호출
+                updateData(currentCursorId,grade,number,name);
+                //화면 리프레시 재생(아래)
+                updateList();
+            }
+        });
+    }
+    //update버튼에 대한 쿼리
+    private void updateData(int currentCursorId, int grade, int number, String name) {
+        //쿼리 매개변수로 1개의 객체에 값을 담기 위해서 객체변수 생성(아래)
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(StudentTable.GRADE,grade);
+        contentValues.put(StudentTable.NUMBER,number);
+        contentValues.put(StudentTable.NAME,name);
+        //SQLiteDatabase에 있는 템플릿메서드중 update사용(아래)
+        mSqLiteDatabase.update(StudentTable.TABLE_NAME,contentValues,StudentTable._ID+"="+currentCursorId,null);
+    }
+
+    //멤버변수로 객체 초기화(아래)
+    private void bindObject() {
+        //EditText변수를 객체로 생성(아래3줄)
+        mEditTextGrade = findViewById(R.id.editTextGrade);
+        mEditTextNumber = findViewById(R.id.editTextNumber);
+        mEditTextName = findViewById(R.id.editTextName);
+        //Button변수를 객체로 생성(아래3줄)
+        mButtonInsert = findViewById(R.id.btnInsert);
+        mButtonUpdate = findViewById(R.id.btnUpdate);
+        mButtonDelete = findViewById(R.id.btnDelete);
+        //키보드제어 객체 생성(아래)
+        mInputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+    }
+    //화면 리프레스
     private void updateList() {
         mItemList.clear();
         mItemList.addAll(getAllData());
@@ -79,9 +218,21 @@ public class MainActivity extends AppCompatActivity {
     private void bindList() {
         //객체 생성
         mRecyclerAdapter = new RecyclerAdapter(mItemList);
+        //어댑터의 OnItemClickListener 추가예정....
+        mRecyclerAdapter.setOnItemClickListener(new RecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                StudentVO studentVO = (StudentVO) mItemList.get(position);
+                //디버그
+                currentCursorId = studentVO.getmId();
+                Toast.makeText(getApplicationContext(),"현재 선택한 커서레코드 ID는 "+currentCursorId,Toast.LENGTH_SHORT).show();
+                mEditTextGrade.setText(Integer.toString(studentVO.getmGrade()));
+                mEditTextNumber.setText(Integer.toString(studentVO.getmNumber()));
+                mEditTextName.setText(studentVO.getmName());
+            }
+        });
         //리사이클러뷰xml과 어댑터 바인딩(attach) No adapter attached
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-
         recyclerView.setHasFixedSize(true);//리사이클러 뷰의 높이를 고정한다.
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(mRecyclerAdapter);//실제 attach(바인딩)
--- app/src/main/java/com/human/sqlite_jeongdoyung/RecyclerAdapter.java
+++ app/src/main/java/com/human/sqlite_jeongdoyung/RecyclerAdapter.java
@@ -17,39 +17,42 @@ import java.util.List;
  * 중첩클래스 만드는법 : RecyclerAdapter.itemViewHoler
  */
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
-    //andloid에서 제공되는 OnitemClick리스너는 매개변수가 4개이기 때문에 2개로 추가
-    public interface OnItemClickListener {//중첩클래스 내에서 사용.
+    //android에서 제공되는 OnItemClick리스너는 매개변수 4개 이기 때문에 2개로 추가
+    public interface OnItemClickListener { //중첩클래스 내에서 사용.
         void onItemClick(View v,int position);//매개변수 2개인 메서드명세 생성.
     }
     //멤버변수
-    private OnItemClickListener mOnItemClickListener;
+    public OnItemClickListener mOnItemClickListener;
     private List mList;
 
-    //온클릭리스너에 대한 셋메서드생성(아래)
-    public void setMonItemClickListener(OnItemClickListener onItemClickListener) {
+    //인터페이스 온클릭리스너에 대한 셋메서드생성(나중에 메인액티비티에서 사용예정, 아래)
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
         mOnItemClickListener = onItemClickListener;
     }
-    //상위 리사이클러 어댑터가 호출되면 자동으로 메서드 실행(아래)
+
+    //ItemViewHolder클래스가 호출되면 자동으로 onCreateViewHolder메서드가 실행됨(아래)
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //레이아웃 xml을 자바단에서 화면을 로딩할때 inflater메서드 사용(아래)
-        //inflate()매개변수 xml은 디자인 필수.
+        //레이아웃 xml을 자바단에서 화면을 로딩할때 inflater메서드 사용(아래)=startActivity(화면을 띄울때)
+        //inflate()매개변수 xml이 디자인이 필수. 리사이클러뷰 내에 들어가는 아이템디자인 생성
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item,parent,false);
         return new ItemViewHolder(view);
     }
-    //생성자 메서드 : 메인액티비티java에서 호출하면서 매개변수 리스트쿼리값을 보내줌.
+
+    //생성자 매서드: 메인액티비티java에서 호출하면서 매개변수 리스트쿼리값을 보냅니다.
     public RecyclerAdapter(List itemList) {
         mList = itemList;//현재 클래스에서 최초로 실행
     }
-    //아래 온바인드뷰홀더에서 mList값을 사용하려면 mList값을 생성(위)
-    //뷰홀더와 포지션pos를 이용해서 xml디자인textView item에 기존값 출력.
-    //개발자가 호출하는것이 아닌 안드로이드의 레이아웃 관리자프로그램이 자동으로 호출.
+
+    //아래 온바인드뷰홀더에서 mList값을 사용하려면, mList값을 생성(위)
+    //뷰홀더와 포지션pos를 이용해서 xml디자인textView item에 기존값 출력하기
+    //개발자가 호출하는 것이 아니고, 안드로이드의 레이아웃 관리자프로그램이 자동으로 호출함.
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-        //구현은 개발자가 코딩(아래)
-        StudentVO studentVO = (StudentVO)mList.get(position);//1개레코드 저장
-        //리사이클러뷰의 recyclerview_item.xml디자인에 데이터를 출력.
+        //구현은 개발자가 코딩함(아래)
+        StudentVO studentVO = (StudentVO) mList.get(position);//1개레코드저장
+        //리사이클러뷰의 recyclerview_item.xml디자인에 데이터를 출력합니다.
         holder.itemGrade.setText(Integer.toString(studentVO.getmGrade()));
         holder.itemNumber.setText(Integer.toString(studentVO.getmNumber()));
         holder.itemName.setText(studentVO.getmName());
@@ -60,33 +63,33 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemVi
         return mList.size();
     }
 
-    //중첩클래스 생성(아래 : 데이터를 홀딩시키는 클래스)
-    public class ItemViewHolder extends RecyclerView.ViewHolder{
-        //멤버변수 선언 : 클래스전역변수
+    //중첩클래스 생성(아래: 리사이클러뷰 콤포넌트에 데이터를 홀딩시키는 클래스)
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        //멤버변수선언:클래스전역변수
         private TextView itemGrade;//홀더내에 객체중 학년 아이템
         private TextView itemNumber;//학번 아이템
         private TextView itemName;//학생이름 아이템
-        public ItemViewHolder(@NonNull View itemView) {//new생성자 메서드
-            super(itemView);//메모리에 ViewHolder클래스를 로딩.
-            //변수 객체로 만듬.(메모리에 로딩 실행가능하게 변경)
-            itemGrade = itemView.findViewById(R.id.item_grade);//xml의 콤포넌트 객체화
+        public ItemViewHolder(@NonNull View itemView) {//new 생성자 메서드
+            super(itemView);//메모리에 ViewHolder클래스를 로딩하게 됨.
+            //변수 객체로 만듬.(메로리에 로딩 = 실행가능하게 변경)
+            itemGrade = itemView.findViewById(R.id.item_grade);//xml의 콤포넌트를 객체화 시킴
             itemNumber = itemView.findViewById(R.id.item_number);
             itemName = itemView.findViewById(R.id.item_name);
-            //new키워드로 생성(객체가 만들어질때)될때 아이템 클릭 이벤트 생성.
+            //new키워드로 생성(객체가만들어질)될때 아이템클릭 이벤트 생성.
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     final int pos = getAdapterPosition();//리사이클러뷰 클릭한 리스트 위치
                     System.out.println("디버그 itemView.setOnClickListener: " + pos);
                     if(pos != RecyclerView.NO_POSITION) {//손가락으로 클릭했을때
-                        //선택한 학생이 없을때 실행(아래)
+                        //선택한 학생이 있을때 실행(아래)
                         if(mOnItemClickListener != null) {
                             mOnItemClickListener.onItemClick(v,pos);
                         }
                     }
                 }
             });
-            //작동확인 후 나중에 람다식(자바버전8이후부터 지원가능)으로 변경.
+            //작동확인 후 나중에 람다식(자바버전8이후부터 지원가능)으로 변경예정.
         }
     }
 }
